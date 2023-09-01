063323ebd694d8dce26f36834b844f0c7be8e923
feenwanwisa <wanwisa.jua@rmutsvmail.com>
2023-09-01 16:35:11 +0700

    update hello.py

--- Lab6_OOP_Concept/Vehicle.py
+++ Lab6_OOP_Concept/Vehicle.py
@@ -1,6 +1,7 @@
 class Vehicle:
     #class attribute
-    #my_vihecle = []
+    my_vehicle = []
+
     def __init__(self,brand,model,color,maxspeed,price):
         self.brand = brand
         self.model = model
@@ -11,12 +12,29 @@ class Vehicle:
 
 
         # difplay object attributes
-    def Vehicle_info(self):
-            print(f'brand:{self.brand} '
+
+    def vehicle_detail(self):
+        print(f'brand:{self.brand} '
                   f'model:{self.model} '
                   f'color:{self.color} '
-                  f'maxspeed:{self.maxspeed}'
-                  f' price:{self.price}')
+                  f'max speed:{self.maxspeed} '
+                  f'price:{self.price}')
+
+    def delete_vehicle(self,index):
+        self.my_vehicle.pop(index)
+
+    def edit_vehicle_price(self,index,new_price):
+        self.my_vehicle[index].price = new_price
+
+    def edit_vehicle_color(self,index,new_color):
+        self.my_vehicle[index].color = new_color
+
+
+
+
+
+
+
 # std = []
 # n = int(input('How attributes Vehicle?: '))
 # for i in range (n):
--- Lab6_OOP_Concept/Vehicle_App.py
+++ Lab6_OOP_Concept/Vehicle_App.py
@@ -4,18 +4,27 @@ from Vehicle import Vehicle
 def display_option():
     print("Welcome to Vehicle Data Store System (VDSS)")
     print("1.Add Vehicle")
-    print("2.Display all Veheicle")
-    print("3.exit")
-    select = int(input("select(1-3) ? : "))
+    print("2.Delete Vehicle")
+    print("3.Edit Vehicle Price")
+    print("4.Edit Vehicle color")
+    print("5.Display all Vehicle")
+    print("6.exit")
+    select = int(input("select(1-6)? : "))
     if select == 1:
         input_vehicle_data()
-    elif select== 2:
+    elif select ==2:
+        delete_vehicle()
+    elif select ==3:
+        edit_vehicle_price()
+    elif select == 4:
+        edit_vehicle_color()
+    elif select == 5:
         display_vehicle()
-    elif select == 3:
+    elif select ==6:
         print("Good Bye.")
         exit(0)
     else:
-        print("Plsas,select number 1-3.")
+        print("Please, select number 1-6.")
 
     # add data
 def input_vehicle_data():
@@ -25,23 +34,70 @@ def input_vehicle_data():
         maxspeed = int(input("Enter vehicle max speed: "))
         price = float(input("Enter vehicle price: "))
 
-        my_vehicle.append(Vehicle(brand,model,color,maxspeed,price))
+        Vehicle.my_vehicle.append(Vehicle(brand,model,color,maxspeed,price))
         print("\n------------------------------------")
         print("Already ass vehicle to sto")
         print("------------------------------------\n")
 
 # display data
 def display_vehicle():
-    if len(my_vehicle) ==0:
+    if len(Vehicle.my_vehicle) ==0:
         print("You had no vehicle data.")
     else:
-        print(f'You had {len(my_vehicle)} following:')
-        for x in  my_vehicle:
+        print(f'You had {len(Vehicle.my_vehicle)} following:')
+        n = 1 # count
+        for x in  Vehicle.my_vehicle:
+            print(f'[{n}]',end=" ")
             x.vehicle_detail()
+            n = n+1
             print("\n")
 
+# delete vehicle
+def delete_vehicle():
+    # display all data in list
+    display_vehicle()
+    if len(Vehicle.my_vehicle) == 0:
+        s = int(input("Select to delete?: "))
+
+
+        Vehicle.delete_vehicle(Vehicle, s-1)
+        print("\n---------------------------------------")
+        print("Your data has been deleted.")
+        print("------------------------------------\n")
+
+# edit vehicle price
+def edit_vehicle_price():
+    display_vehicle()
+    if len(Vehicle.my_vehicle) != 0:
+        s = int(input("Select to edit?: "))
+        # display previous price
+        print(f'previous price: {Vehicle.my_vehicle[s-1].price}')
+        new_price = float(input("new price: "))
+        Vehicle.edit_vehicle_price(Vehicle,s-1,new_price)
+        print("\n---------------------------------------")
+        print("Your data has been edited.")
+        print("------------------------------------\n")
+
+# edit vehicle price
+def edit_vehicle_color():
+    display_vehicle()
+    if len(Vehicle.my_vehicle) != 0:
+        s = int(input("Select to edit?: "))
+        # display previous color
+        print(f'previous color: {Vehicle.my_vehicle[s-1].color}')
+        new_color = input("new color: ")
+        Vehicle.edit_vehicle_color(Vehicle,s-1,new_color)
+        print("\n---------------------------------------")
+        print("Your data has been edited.")
+        print("------------------------------------\n")
+
+
+
+
+
+
 # run
-my_vehicle = []
+#my_vehicle = []
 s = 0
 while s == 0:
     display_option()
